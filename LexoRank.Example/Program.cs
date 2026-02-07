7eafa90f7ee54b1cac053308bf98dbfe7d563109
tsctsc6 <tsctsc6@126.com>
2026-02-07 17:48:45 +0800

    Rename proj; ignore .user file

---
+++ LexoRank.Example/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using LexoRank.Core;
+
+var lexoRankManager = new LexoRankManager("0123456789");
+
+var sortedList = new SortedDictionary<string, string>();
+var sortedValue = string.Empty;
+for (var i = 0; i < 10; i++)
+{
+    // sortedValue = lexoRankManager.Between(sortedValue, string.Empty);
+    sortedValue = lexoRankManager.Between(string.Empty, sortedValue);
+    sortedList.Add(sortedValue, $"item{i}");
+}
+
+Console.WriteLine(string.Join(Environment.NewLine, sortedList));
