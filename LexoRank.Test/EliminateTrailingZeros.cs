70beaacd0b35e5afa648449503d9570c5fb05da4
tsctsc6 <tsctsc6@126.com>
2026-02-07 18:31:06 +0800

    write up unit test

---
+++ LexoRank.Test/EliminateTrailingZeros.cs
@@ -0,0 +1,22 @@
+﻿using LexoRank.Core;
+
+namespace LexoRank.Test;
+
+public class EliminateTrailingZeros
+{
+    [Fact]
+    public void Normal()
+    {
+        var lexoRankManager = new LexoRankManager("0123456789");
+        var sortedValue = lexoRankManager.Between("10721", "29279");
+        Assert.Equal("2", sortedValue);
+        var sortedList = new SortedDictionary<string, int>();
+        sortedList.Add("29279", 2);
+        sortedList.Add("10721", 0);
+        sortedList.Add("2", 1);
+        var sortedArray = sortedList.ToArray();
+        Assert.Equal(0, sortedArray[0].Value);
+        Assert.Equal(1, sortedArray[1].Value);
+        Assert.Equal(2, sortedArray[2].Value);
+    }
+}
