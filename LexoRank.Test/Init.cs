2bf39fab8db1e09fdc2ddabfb2efcc3a51483e1b
tsctsc6 <tsctsc6@126.com>
2026-02-07 18:04:07 +0800

    Add unit test

---
+++ LexoRank.Test/Init.cs
@@ -0,0 +1,14 @@
+﻿using LexoRank.Core;
+
+namespace LexoRank.Test;
+
+public class Init
+{
+    [Fact]
+    public void Normal()
+    {
+        var lexoRankManager = new LexoRankManager("0123456789");
+        var sortedValue = lexoRankManager.Between(string.Empty, string.Empty);
+        Assert.Equal("5", sortedValue);
+    }
+}
