c540c156ef0660902f09c49b832ef266dd7b5bc9
tsctsc6 <tsctsc6@126.com>
2026-02-09 20:12:18 +0800

    Add benchmark

---
+++ LexoRank.Benchmark/Program.cs
@@ -0,0 +1,26 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using LexoRank.Core;
+
+namespace LexoRank.Benchmark;
+
+public class MyBenchmark
+{
+    private readonly LexoRankManager _lexoRankManager = new(CommonCharacterSets.Base62);
+
+    [Benchmark]
+    public void Normal()
+    {
+        var sortedValue0 = "nopSDGpa51g6f1asd3g13SDFGf65g1a3dgfE345FGYMNPM0mlm";
+        var sortedValue1 = "56145fSFD6A6D161ASD41fa6df19ADF61fa66asdf44tas233";
+        _ = _lexoRankManager.Between(sortedValue0, sortedValue1).Unwrap();
+    }
+}
+
+public static class Program
+{
+    public static void Main()
+    {
+        BenchmarkRunner.Run<MyBenchmark>();
+    }
+}
