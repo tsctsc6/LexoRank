cb8ec0e77122bf9d6a6b144e8ca4a6e62a544ce5
tsctsc6 <tsctsc6@126.com>
2026-02-09 20:21:42 +0800

    add unit test

---
+++ LexoRank.Test/EmptyCharacterSet.cs
@@ -0,0 +1,24 @@
+﻿using LexoRank.Core;
+using LexoRank.Core.Errors;
+using RustSharp;
+
+namespace LexoRank.Test;
+
+public class EmptyCharacterSet
+{
+    [Fact]
+    public void Normal()
+    {
+        var lexoRankManager = new LexoRankManager(string.Empty);
+        var result = lexoRankManager.Between(string.Empty, string.Empty);
+        switch (result)
+        {
+            case ErrResult<string, List<Error>>:
+                Assert.True(true);
+                break;
+            default:
+                Assert.Fail();
+                break;
+        }
+    }
+}
---
+++ LexoRank.Test/InvalidLexoRankString.cs
@@ -0,0 +1,24 @@
+﻿using LexoRank.Core;
+using LexoRank.Core.Errors;
+using RustSharp;
+
+namespace LexoRank.Test;
+
+public class InvalidLexoRankString
+{
+    [Fact]
+    public void Normal()
+    {
+        var lexoRankManager = new LexoRankManager(CommonCharacterSets.Digits);
+        var result = lexoRankManager.Between("abc", string.Empty);
+        switch (result)
+        {
+            case ErrResult<string, List<Error>>:
+                Assert.True(true);
+                break;
+            default:
+                Assert.Fail();
+                break;
+        }
+    }
+}
