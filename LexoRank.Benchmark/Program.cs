48579faf1bcb17bbd1304fe3ac43d186d52381ac
tsctsc6 <tsctsc6@126.com>
2026-02-11 16:11:35 +0800

    Update Benchmark

--- LexoRank.Benchmark/Program.cs
+++ LexoRank.Benchmark/Program.cs
@@ -7,13 +7,19 @@ namespace LexoRank.Benchmark;
 public class MyBenchmark
 {
     private readonly LexoRankManager _lexoRankManager = new(CommonCharacterSets.Base62);
+    private readonly string _sortedValue0 = Random.Shared.GetString(
+        CommonCharacterSets.Base62,
+        100
+    );
+    private readonly string _sortedValue1 = Random.Shared.GetString(
+        CommonCharacterSets.Base62,
+        100
+    );
 
     [Benchmark]
     public void Normal()
     {
-        var sortedValue0 = "nopSDGpa51g6f1asd3g13SDFGf65g1a3dgfE345FGYMNPM0mlm";
-        var sortedValue1 = "56145fSFD6A6D161ASD41fa6df19ADF61fa66asdf44tas233";
-        _ = _lexoRankManager.Between(sortedValue0, sortedValue1);
+        _ = _lexoRankManager.Between(_sortedValue0, _sortedValue1);
     }
 }
 
