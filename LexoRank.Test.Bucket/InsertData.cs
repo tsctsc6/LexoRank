b847b0246deae28db80f7cc13c9943861195aa11
tsctsc6 <tsctsc6@126.com>
2026-02-12 16:29:57 +0800

    write the code for SeedingData

--- LexoRank.Test.Bucket/InsertData.cs
+++ LexoRank.Test.Bucket/InsertData.cs
@@ -1,6 +1,5 @@
 ﻿using LexoRank.Core;
 using LexoRank.Test.Bucket.Database;
-using LexoRank.Test.Bucket.Database.Entities;
 using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
 using Xunit;
@@ -24,32 +23,7 @@ public class InsertData
             ["0", "1", "2"]
         );
 
-        var post1 = new Post()
-        {
-            Id = 1,
-            Title = "Post1",
-            SortingValue = lexoRankBucketManager.Between(string.Empty, string.Empty),
-        };
-        var post2 = new Post()
-        {
-            Id = 2,
-            Title = "Post2",
-            SortingValue = lexoRankBucketManager.Between(post1.SortingValue, string.Empty),
-        };
-        var post3 = new Post()
-        {
-            Id = 3,
-            Title = "Post3",
-            SortingValue = lexoRankBucketManager.Between(post1.SortingValue, post2.SortingValue),
-        };
-        var post4 = new Post()
-        {
-            Id = 4,
-            Title = "Post4",
-            SortingValue = lexoRankBucketManager.Between(string.Empty, post1.SortingValue),
-        };
-        await dbContext.Posts.AddRangeAsync(post1, post2, post3, post4);
-        await dbContext.SaveChangesAsync(TestContext.Current.CancellationToken);
+        await Preparation.SeedingDataAsync(lexoRankBucketManager, dbContext);
 
         var result = await dbContext
             .Posts.OrderBy(p => p.SortingValue)
