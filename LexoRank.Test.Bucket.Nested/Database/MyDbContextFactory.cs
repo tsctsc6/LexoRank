606cd0fbe3456c419036a378886ebc9578087b4a
tsctsc6 <tsctsc6@126.com>
2026-02-15 21:08:57 +0800

    write up MyDbContextFactory

--- LexoRank.Test.Bucket.Nested/Database/MyDbContextFactory.cs
+++ LexoRank.Test.Bucket.Nested/Database/MyDbContextFactory.cs
@@ -1,19 +1,30 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Design;
 using Microsoft.Extensions.Configuration;
 
 namespace LexoRank.Test.Bucket.Nested.Database;
 
-public class MyDbContextFactory : IDesignTimeDbContextFactory<MyDbContext>
+public class MyDbContextFactory
 {
     private readonly IConfigurationRoot _config = new ConfigurationBuilder()
         .AddJsonFile("appsettings.Testing.json", optional: false, reloadOnChange: true)
         .Build();
 
-    public MyDbContext CreateDbContext(string[] args)
+    public string DbName { get; } = "lexorank_test_" + Guid.NewGuid().ToString("N");
+
+    public MyDbContext CreateDbContext()
     {
         var options = new DbContextOptionsBuilder<MyDbContext>()
-            .UseNpgsql(_config["ConnectionStrings:Default"])
+            .UseNpgsql(
+                $"{_config["ConnectionStrings:Default"]}Database={DbName}"
+                // npgsqlOptions =>
+                // {
+                //     npgsqlOptions.EnableRetryOnFailure(
+                //         maxRetryCount: 3,
+                //         maxRetryDelay: TimeSpan.FromMilliseconds(50),
+                //         errorCodesToAdd: ["40001"]
+                //     );
+                // }
+            )
             .Options;
         return new MyDbContext(options);
     }
